b360b663e1bdbdd36ad2778c2bdf681cad104c07
herimaharavohaja <hei.haja.25@gmail.com>
2024-05-22 10:09:46 +0200

    chore: configuration database

---
+++ kaly/src/main/java/com/restauration/kaly/DbConfig/ConfigDb.java
@@ -0,0 +1,27 @@
+package com.restauration.kaly.DbConfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Configuration
+public class ConfigDb {
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.user}")
+    private String dbUser;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Bean
+    public Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+    }
+}
