6e6e7ba9bb88bd7f6042fa99b195ab9535b0f719
herimaharavohaja <hei.haja.25@gmail.com>
2024-05-22 08:17:15 +0200

    feat: add product

---
+++ kaly/src/main/java/com/restauration/kaly/KalyApplication.java
@@ -0,0 +1,13 @@
+package com.restauration.kaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalyApplication.class, args);
+	}
+
+}
